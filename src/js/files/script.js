4a9e2c0da25624b38a2c148e98f8dd0324e82f7c
VirtoOZ <109903427+VirtoOZ@users.noreply.github.com>
2024-07-17 22:31:55 +0300

    .get-started adaptive end

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,11 +1,13 @@
 //<BURGER>=================================
 const iconMenu = document.querySelector('.menu__icon');//находим класс menu__icon
 const menuBody = document.querySelector('.menu__body');//находим класс menu__body
+const headerBody = document.querySelector('.header__body');//находим класс menu__body
 if (iconMenu) {//Проверяем есть ли menu__icon
 	iconMenu.addEventListener("click", function (e) {//вещам событие при клике мыши
 		document.body.classList.toggle('_lock');//Для Body даем класс Lock для отключения прокрутки
 		iconMenu.classList.toggle('_active');//добавляем класс active menu__icon
 		menuBody.classList.toggle('_active');//добавляем класс active menu__body
+		headerBody.classList.toggle('_active');//добавляем класс active menu__body
 		// _slideToggle(menuBody, 500);
 	});
 };
